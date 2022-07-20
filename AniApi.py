705c1d34ae1a755d6c58595b57cda9372d5112d5
Tenshi Kira <garciarodrigueza587@gmail.com>
2022-07-19 18:18:32 -0600

    agrego datos tipo int a str

--- AniApi.py
+++ AniApi.py
@@ -9,7 +9,7 @@ import requests
 import json
 
 if __name__ == '__main__':
-
+  
     os.system('clear')
 
     name = Style.BRIGHT + Fore.MAGENTA + """           AniMencion.Api \n """
@@ -31,10 +31,22 @@ if __name__ == '__main__':
     print(respuesta['title'])
     print("")
     time.sleep(2)
+    #print("Estudio"+ str(respuesta['studios']))
+    print(Style.BRIGHT + Fore.BLUE + "Fans: "+ Fore.YELLOW + str(respuesta['members']))
+    print(Style.BRIGHT + Fore.BLUE + "Puntuacion: "+ Fore.YELLOW + str(respuesta['score']))
+    print("")
+    time.sleep(2)
+    print(Style.BRIGHT + Fore.BLUE +"Fecha: "+ Fore.YELLOW + str(respuesta["aired"]))
+    print("")
     print(Style.BRIGHT + Fore.BLUE + 'Estado: '+ Fore.YELLOW + respuesta['status'])
     print("")
     time.sleep(2.1)
     print(Style.BRIGHT + Fore.BLUE + 'Clasificacion: '+ Fore.YELLOW + respuesta['rating'])
+    print("")
+    time.sleep(2.1)
+    print(Style.BRIGHT + Fore.BLUE + "Popular: "+ Fore.YELLOW + str(respuesta['popularity']))
+    print("")
+    print(Style.BRIGHT + Fore.BLUE + 'Episodios '+ Fore.YELLOW + str(respuesta['episodes']))
     print("")
     time.sleep(2.1)
     print(Style.BRIGHT + Fore.BLUE + 'Duracion: '+ Fore.YELLOW + respuesta['duration'])
@@ -49,7 +61,8 @@ if __name__ == '__main__':
     print("")
     time.sleep(2.1)
     print(Style.BRIGHT + Fore.BLUE + 'Tipo: '+ Fore.YELLOW + respuesta['type'])
-    print("")
+    print("") 
+    #print(Style.BRIGHT + Fore.BLUE + 'Episodios '+ Fore.YELLOW + str(respuesta['episodes']))
     time.sleep(2.1)
     print(Style.BRIGHT + Fore.MAGENTA + """                SYNOPSIS                   """)
     time.sleep(2.4)
