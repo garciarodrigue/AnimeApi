bcb5f4a7f8600a86f794144cfb0f3a7502e6bff6
Tenshi Kira <garciarodrigueza587@gmail.com>
2022-08-06 18:39:07 -0600

    while true

--- AniApi.py
+++ AniApi.py
@@ -78,56 +78,66 @@ if __name__ == '__main__':
           """) 
     #print(Style.BRIGHT + Fore.BLUE + 'Episodios '+ Fore.YELLOW + str(respuesta['episodes']))
     time.sleep(2.1)
-if __name__ == '__main__':
-    def pedirDatos():   
-        elige = input(Style.BRIGHT + Fore.RED + "Escribe el tipo de Inf:➽  " + Fore.GREEN)
+#if __name__ == '__main__':
+    #def pedirDatos():  
+def datos():
+    while True:
+        try:
+            elige = input(Style.BRIGHT + Fore.RED + "Escribe el tipo de Inf:➽  " + Fore.GREEN)
 
-        if elige == "tipo":
-            print("\n" + tipo)
+            if elige == "tipo":
+                print("\n" + tipo)
     
-        elif elige == "clasificacion":
-            print("\n" + clasificacion)
+            elif elige == "clasificacion":
+                print("\n" + clasificacion)
     
-        elif elige == "episodios":
-            print("\n" + episodios)
+            elif elige == "episodios":
+                print("\n" + episodios)
 
-        elif elige == "fecha":
-            print("\n" + fecha)
+            elif elige == "fecha":
+                print("\n" + fecha)
 
-        elif elige == "web":
-            print("\n" + web)
+            elif elige == "web":
+                print("\n" + web)
 
-        elif elige == "inicio":
-            print("\n" + inicio)
+            elif elige == "inicio":
+                print("\n" + inicio)
 
-        elif elige == "popular":
-            print("\n" + popular)
+            elif elige == "popular":
+                print("\n" + popular)
 
-        elif elige == "fans":
-            print("\n" + fans)
+            elif elige == "fans":
+                print("\n" + fans)
         
-        elif elige == "duracion":
-            print("\n" + duracion)
+            elif elige == "duracion":
+                print("\n" + duracion)
 
-        elif elige == "puntuacion":
-            print("\n" + puntuacion)
+            elif elige == "puntuacion":
+                print("\n" + puntuacion)
 
-        elif elige == "estado":
-            print("\n" + estado)
+            elif elige == "estado":
+                print("\n" + estado)
             
 
-        elif elige == "synopsis":
-            print(Style.BRIGHT + Fore.MAGENTA + """\n              ◉ SYNOPSIS ◎                   """)
-            time.sleep(2.4)
-            name = Style.BRIGHT + Back.WHITE + Fore.BLACK + respuesta['synopsis']
+            elif elige == "synopsis":
+                print(Style.BRIGHT + Fore.MAGENTA + """\n              ◉ SYNOPSIS ◎                   """)
+                time.sleep(2.4)
+                name = Style.BRIGHT + Back.WHITE + Fore.BLACK + respuesta['synopsis\n'] + Back.BLACK
     
-            for l in name:
-                sys.stdout.flush()
-                print(l,end="")
-                time.sleep(0.1)
-        else:
-            print(Style.BRIGHT + Fore.RED + "\nError\nAsegurate de aver Escrito bien")
-    pedirDatos();
+                for l in name:
+                    sys.stdout.flush()
+                    print(l,end="")
+                    time.sleep(0.1)
+            else:
+                print(Style.BRIGHT + Fore.CYAN + "\nError\nAsegurate de aver Escrito bien\n")
+    #pedirDatos();
     #print("adaptacion: "+ respuesta['Adaptation'])
-
-
+            break
+        except ValueError:
+            print(Fore.RED ,"\ninfo no coinside\n")
+datos()
+while True:
+    try:
+        datos()
+    except ValueError:
+        datos()
